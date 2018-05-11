181febfa15454a99e88aff6a5bfc6bbef81e0059
林澄南 <linchengnan@aobi.com>
2018-05-11 10:42:06 +0800

    tkinter example

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,165 +1,37 @@
-import urllib
+from tkinter import *
+from tkinter import ttk
 
-from matplotlib import style
-from matplotlib.finance import candlestick_ohlc
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+    
+root = Tk()
+root.title("Feet to Meters")
 
-import datetime as dt
-import matplotlib as mpl
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
-import numpy as np
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
 
+feet = StringVar()
+meters = StringVar()
 
-mpl.use('Qt5Agg')
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
 
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
 
-style.use('fivethirtyeight')
-print(plt.style.available)
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
-print(plt.__file__)
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
-MA1 = 10
-MA2 = 30
+feet_entry.focus()
+root.bind('<Return>', calculate)
 
-def ma(values, window):
-    weights = np.repeat(1.0, window)/window
-    smas = np.convolve(values, weights, 'valid')
-    return smas
-
-def high_minus_low(highs, lows):
-    return highs-lows
-
-
-def bytespdate2num(fmt, encoding='utf-8'):
-    strconverter = mdates.strpdate2num(fmt)
-    def bytesconverter(b):
-        s = b.decode(encoding)
-        return strconverter(s)
-    return bytesconverter
-
-
-def graph_data(stock):
-
-    fig = plt.figure(facecolor='#f0f0f0')
-    ax1 = plt.subplot2grid((6,1), (0,0), rowspan=1, colspan=1)
-    plt.title(stock)
-    plt.ylabel('H-L')
-    ax_volume = plt.subplot2grid((6,1), (1,0), rowspan=4, colspan=1, sharex=ax1)
-    plt.ylabel('Price')
-    ax2v = ax_volume.twinx()
-
-    ax3 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
-    plt.ylabel('MAvgs')
-
-
-    stock_price_url = 'http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range=1y/csv'
-    source_code = urllib.request.urlopen(stock_price_url).read().decode()
-    stock_data = []
-    split_source = source_code.split('\n')
-    for line in split_source:
-        split_line = line.split(',')
-        if len(split_line) == 6:
-            if 'values' not in line and 'labels' not in line:
-                stock_data.append(line)
-
-
-    date, closep, highp, lowp, openp, volume = np.loadtxt(stock_data,
-                                                          delimiter=',',
-                                                          unpack=True,
-                                                          converters={0: bytespdate2num('%Y%m%d')})
-
-    lows = 0
-    y = len(date)
-    ohlc = []
-
-    while lows < y:
-        append_me = date[lows], openp[lows], highp[lows], lowp[lows], closep[lows], volume[lows]
-        ohlc.append(append_me)
-        lows+=1
-
-    ma1 = ma(closep,MA1)
-    ma2 = ma(closep,MA2)
-    start = len(date[MA2-1:])
-
-    h_l = list(map(high_minus_low, highp, lowp))
-
-
-    ax1.plot_date(date[-start:],h_l[-start:],'-', label='H-L')
-    ax1.yaxis.set_major_locator(mticker.MaxNLocator(nbins=4, prune='lower'))
-
-
-    candlestick_ohlc(ax_volume, ohlc[-start:], width=0.4, colorup='#77d879', colordown='#db3f3f')
-
-
-
-    ax_volume.yaxis.set_major_locator(mticker.MaxNLocator(nbins=7, prune='upper'))
-    ax_volume.grid(True)
-
-    bbox_props = dict(boxstyle='round',fc='w', ec='k',lw=1)
-
-    ax_volume.annotate(str(closep[-1]), (date[-1], closep[-1]),
-                 xytext = (date[-1]+4, closep[-1]), bbox=bbox_props)
-
-##    # Annotation example with arrow
-##    ax_volume.annotate('Bad News!',(date[11],highp[11]),
-##                 xytext=(0.8, 0.9), textcoords='axes fraction',
-##                 arrowprops = dict(facecolor='grey',color='grey'))
-##
-##    
-##    # Font dict example
-##    font_dict = {'family':'serif',
-##                 'color':'darkred',
-##                 'size':15}
-##    # Hard coded text 
-##    ax_volume.text(date[10], closep[1],'Text Example', fontdict=font_dict)
-
-    ax2v.plot([],[], color='#0079a3', alpha=0.4, label='Volume')
-    ax2v.fill_between(date[-start:],0, volume[-start:], facecolor='#0079a3', alpha=0.4)
-    ax2v.axes.yaxis.set_ticklabels([])
-    ax2v.grid(False)
-    ax2v.set_ylim(0, 3*volume.max())
-
-
-
-    ax3.plot(date[-start:], ma1[-start:], linewidth=1, label=(str(MA1)+'MA'))
-    ax3.plot(date[-start:], ma2[-start:], linewidth=1, label=(str(MA2)+'MA'))
-
-    ax3.fill_between(date[-start:], ma2[-start:], ma1[-start:],
-                     where=(ma1[-start:] < ma2[-start:]),
-                     facecolor='r', edgecolor='r', alpha=0.5)
-
-    ax3.fill_between(date[-start:], ma2[-start:], ma1[-start:],
-                     where=(ma1[-start:] > ma2[-start:]),
-                     facecolor='g', edgecolor='g', alpha=0.5)
-
-    ax3.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    ax3.xaxis.set_major_locator(mticker.MaxNLocator(10))
-    ax3.yaxis.set_major_locator(mticker.MaxNLocator(nbins=4, prune='upper'))
-
-    for label in ax3.xaxis.get_ticklabels():
-        label.set_rotation(45)
-
-
-
-    plt.setp(ax1.get_xticklabels(), visible=False)
-    plt.setp(ax_volume.get_xticklabels(), visible=False)
-    plt.subplots_adjust(left=0.11, bottom=0.24, right=0.90, top=0.90, wspace=0.2, hspace=0)
-
-    ax1.legend()
-    leg = ax1.legend(loc=9, ncol=2,prop={'size':11})
-    leg.get_frame().set_alpha(0.4)
-
-    ax2v.legend()
-    leg = ax2v.legend(loc=9, ncol=2,prop={'size':11})
-    leg.get_frame().set_alpha(0.4)
-
-    ax3.legend()
-    leg = ax3.legend(loc=9, ncol=2,prop={'size':11})
-    leg.get_frame().set_alpha(0.4)
-
-    plt.show()
-    fig.savefig('google.png', facecolor=fig.get_facecolor())
-
-
-graph_data('GOOG')
+root.mainloop()
